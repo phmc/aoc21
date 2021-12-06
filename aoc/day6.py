09ccd886a1d9ee7ac5771cf40d82b12ec1a9c570
Phil Connell <pconnell@gmail.com>
2021-12-06 09:30:24 +0000

    Day 6, part 2

--- aoc/day6.py
+++ aoc/day6.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python3
 
+import collections
 import sys
-from typing import Iterable, Iterator
+from typing import Iterable, Iterator, Mapping
 
 
-def _get_next_days_timers(timers: Iterable[int]) -> Iterator[int]:
-    """Yield values for tomorrow's timers, based on today's timers."""
-    for timer in timers:
-        if not timer:
-            yield 6
-            yield 8
-        else:
-            assert timer > 0
-            yield timer - 1
+def _get_next_days_timers(today: collections.Counter[int]) -> collections.Counter[int]:
+    """
+    Return values for tomorrow's timers, based on today's timers.
+
+    Input and output count the number of timers with each value.
+
+    """
+    tomorrow = collections.Counter(
+        {value - 1: count for value, count in today.items() if value > 0}
+    )
+    expiring = today.get(0, 0)
+    tomorrow.update({6: expiring, 8: expiring})
+    return tomorrow
+
+
+def _get_n_days_timers(
+    today: collections.Counter[int], n: int
+) -> collections.Counter[int]:
+    """Return timer counts advanced n days."""
+    counts = today
+    for _ in range(n):
+        counts = _get_next_days_timers(counts)
+    return counts
 
 
 def _parse_initial_timers(lines: Iterable[str]) -> Iterator[int]:
@@ -24,10 +39,11 @@ def _parse_initial_timers(lines: Iterable[str]) -> Iterator[int]:
 
 def main(argv: list[str]) -> None:
     with open(argv[0]) as f:
-        timers = _parse_initial_timers(f)
-        for _ in range(80):
-            timers = _get_next_days_timers(timers)
-        print(len(list(timers)))
+        counts = collections.Counter(_parse_initial_timers(f))
+        counts = _get_n_days_timers(counts, n=80)
+        print(sum(counts.values()))
+        counts = _get_n_days_timers(counts, n=(256 - 80))
+        print(sum(counts.values()))
 
 
 if __name__ == "__main__":
