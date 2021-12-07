fe380db3a026325809083c815ccc21b161e6df21
Phil Connell <pconnell@gmail.com>
2021-12-07 09:42:59 +0000

    Day 7, part 2

--- aoc/day7.py
+++ aoc/day7.py
@@ -1,15 +1,60 @@
 #!/usr/bin/env python3
 
+import functools
 import sys
-from typing import Iterable, Iterator
+from typing import Iterable, Iterator, Optional
 
 
-def _find_middle_position(positions: Iterable[int]) -> int:
-    """Return the middle position among the given positions."""
+def _calculate_linear_cost(target: int, positions: Iterable[int]) -> int:
+    """Return the total cost of moving to the target position for a linear metric."""
+    return sum(abs(pos - target) for pos in positions)
+
+
+def _calculate_linear_optimum(positions: Iterable[int]) -> int:
+    """Return the optimal position for a linear metric."""
     positions = sorted(positions)
+    # It's the median!
+    #
     # In the case where there are an even number of starting positions, the
     # middle two positions have the same cost so arbitrarily choose the first.
     return positions[(len(positions) + 1) // 2]
+
+
+@functools.cache
+def _triangular(n: int) -> int:
+    """Return the nth triangular number."""
+    return n * (n + 1) // 2
+
+
+def _calculate_triangular_cost(target: int, positions: Iterable[int]) -> int:
+    """Return the total cost of moving to the target position for a triangular metric."""
+    return sum(_triangular(abs(pos - target)) for pos in positions)
+
+
+def _calculate_triangular_optimum(positions: Iterable[int]) -> int:
+    """Return the optimal position for a triangular metric."""
+    # Assume that there is no local (but not global) minimum, and bisect.
+    # Haven't proved this...
+    positions = list(positions)
+    lower = min(positions)
+    lower_cost = _calculate_triangular_cost(lower, positions)
+    upper = max(positions)
+    upper_cost = _calculate_triangular_cost(upper, positions)
+
+    while upper - lower > 1:
+        if lower_cost <= upper_cost:
+            # Selecting the lower half, so prefer a higher new upper bound.
+            upper = (upper + lower + 1) // 2
+            upper_cost = _calculate_triangular_cost(upper, positions)
+        else:
+            # Vice-versa, prefer a lower value.
+            lower = (upper + lower) // 2
+            lower_cost = _calculate_triangular_cost(lower, positions)
+
+    if lower_cost < upper_cost:
+        return lower
+    else:
+        return upper
 
 
 def _parse_inputs(lines: Iterable[str]) -> Iterator[int]:
@@ -22,8 +67,10 @@ def _parse_inputs(lines: Iterable[str]) -> Iterator[int]:
 def main(argv: list[str]) -> None:
     with open(argv[0]) as f:
         starts = list(_parse_inputs(f))
-        middle = _find_middle_position(starts)
-        print(sum(abs(pos - middle) for pos in starts))
+        target = _calculate_linear_optimum(starts)
+        print(_calculate_linear_cost(target, starts))
+        target = _calculate_triangular_optimum(starts)
+        print(_calculate_triangular_cost(target, starts))
 
 
 if __name__ == "__main__":
