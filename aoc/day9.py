1282a39a47b4177394cb9d89ccc2e7a4227b7447
Phil Connell <pconnell@gmail.com>
2021-12-09 20:22:56 +0000

    Day 9, part 2

--- aoc/day9.py
+++ aoc/day9.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import collections
+import functools
+import operator
 import sys
 
 from typing import Iterable, Iterator
@@ -28,18 +30,28 @@ def _get_neighbours(m: Map, p: Point) -> set[Point]:
     }
 
 
-def _get_neighbour_heights(m: Map, p: Point) -> set[int]:
-    """Return heights of a point's neighbours."""
-    return {m[neighbour] for neighbour in _get_neighbours(m, p)}
-
-
 def _find_low_points(m: Map) -> Iterator[Point]:
     """Yield all low points in a map."""
     return (
         point
-        for point, height in m.items()
-        if all(height < n_height for n_height in _get_neighbour_heights(m, point))
+        for point in m
+        if all(m[point] < m[neighbour] for neighbour in _get_neighbours(m, point))
     )
+
+
+def _find_basin(m: Map, low_point: Point) -> Iterator[Point]:
+    """Yield all points in the basin draining to a low point."""
+    candidates: set[Point] = set([low_point])
+    visited: set[Point] = set()
+    while candidates:
+        current = candidates.pop()
+        visited.add(current)
+        yield current
+        candidates.update(
+            neighbour
+            for neighbour in _get_neighbours(m, current)
+            if neighbour not in visited and m[current] < m[neighbour] < 9
+        )
 
 
 def _parse_input(lines: Iterable[str]) -> Map:
@@ -55,7 +67,11 @@ def _parse_input(lines: Iterable[str]) -> Map:
 def main(argv: list[str]) -> None:
     with open(argv[0]) as f:
         m = _parse_input(f)
-        print(sum(1 + m[point] for point in _find_low_points(m)))
+        low_points = set(_find_low_points(m))
+        print(sum(1 + m[point] for point in low_points))
+        basins = (_find_basin(m, low_point) for low_point in low_points)
+        sizes = (len(list(basin)) for basin in basins)
+        print(functools.reduce(operator.mul, sorted(sizes)[-3:]))
 
 
 if __name__ == "__main__":
