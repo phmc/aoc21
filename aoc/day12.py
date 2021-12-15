83bd9617bfe83b88156257e1c036d05dbb6e5d6c
Phil Connell <pconnell@gmail.com>
2021-12-15 09:25:21 +0000

    Approaching the cheesier end of the micro-optimisation spectrum

--- aoc/day12.py
+++ aoc/day12.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 import collections
-import functools
 import sys
 import typing
 from typing import Iterable
@@ -13,11 +12,21 @@ Cave = typing.NewType("Cave", str)
 # Mapping from caves to neighbours.
 Map = dict[Cave, set[Cave]]
 
-_START = Cave("start")
-_END = Cave("end")
+# Singleton instances of caves, allowing comparison by identity.
+_CAVES: dict[str, Cave] = {}
 
 
-@functools.cache
+def _make_cave(id: str) -> Cave:
+    """Create a cave, or reuse the existing cave with the same ID."""
+    if id not in _CAVES:
+        _CAVES[id] = Cave(id)
+    return _CAVES[id]
+
+
+_START = _make_cave("start")
+_END = _make_cave("end")
+
+
 def _is_big(cave: Cave) -> bool:
     """Return `True` if this cave is big."""
     return cave.isupper()
@@ -26,17 +35,22 @@ def _is_big(cave: Cave) -> bool:
 def _count_paths(m: Map, may_ever_revisit_small: bool) -> int:
     """Count the number of paths from start to end."""
     complete_paths = 0
+    small_caves = set(
+        cave
+        for cave in m
+        if not _is_big(cave) and cave is not _START and cave is not _END
+    )
     pending: list[tuple[bool, frozenset[Cave], Cave]] = [
         (may_ever_revisit_small, frozenset(), _START)
     ]
     while pending:
         may_revisit_small, visited, cave = pending.pop()
         for neighbour in m[cave]:
-            if neighbour == _END:
+            if neighbour is _END:
                 complete_paths += 1
-            elif neighbour == _START:
+            elif neighbour is _START:
                 continue
-            elif _is_big(neighbour):
+            elif neighbour not in small_caves:
                 pending.append((may_revisit_small, visited, neighbour))
             elif neighbour not in visited:
                 pending.append(
@@ -46,7 +60,7 @@ def _count_paths(m: Map, may_ever_revisit_small: bool) -> int:
                         neighbour,
                     )
                 )
-            elif may_revisit_small and neighbour != _START:
+            elif may_revisit_small:
                 pending.append(
                     (
                         False,
@@ -61,7 +75,7 @@ def _parse_input(lines: Iterable[str]) -> Map:
     """Parse a cave map from input lines."""
     m: Map = collections.defaultdict(set)
     for line in lines:
-        first, second = (Cave(tok) for tok in line.strip().split("-"))
+        first, second = (_make_cave(tok) for tok in line.strip().split("-"))
         m[first].add(second)
         m[second].add(first)
     return dict(m)
