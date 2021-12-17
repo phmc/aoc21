f6a099da2ade7819da15013f66985df3a5be0d2f
Phil Connell <pconnell@gmail.com>
2021-12-17 13:34:38 +0000

    Day 17, part 2

--- aoc/day17.py
+++ aoc/day17.py
@@ -3,7 +3,7 @@
 import collections
 import dataclasses
 import sys
-from typing import Iterable, Iterator, Optional
+from typing import Iterable, Iterator
 
 
 Point = collections.namedtuple("Point", ["x", "y"])
@@ -28,19 +28,11 @@ class Area:
         return self.min_x <= p.x <= self.max_x and self.min_y <= p.y <= self.max_y
 
 
-def _next_x_speed(speed: int) -> int:
-    """Return the speed in the X direction for the next step."""
-    if speed == 0:
-        return 0
-    else:
-        return abs(speed - 1) * speed // abs(speed)
-
-
 def _step(p: Point, v: Velocity) -> tuple[Point, Velocity]:
     """Return the new position and velocity after a step."""
     return (
         Point(p.x + v.x, p.y + v.y),
-        Velocity(_next_x_speed(v.x), v.y - 1),
+        Velocity(0 if not v.x else abs(v.x - 1) * v.x // abs(v.x), v.y - 1),
     )
 
 
@@ -74,45 +66,32 @@ def _fire(initial_p: Point, initial_v: Velocity, target: Area) -> Point:
     return collections.deque(_trajectory(initial_p, initial_v, target), maxlen=1)[0]
 
 
-def _find_suitable_x_speed(
-    initial_p: Point, initial_y_speed: int, target: Area
-) -> Optional[int]:
-    """Find an initial speed in the x direction that hits the target."""
-    # An upper bound is abs(target.max_x), so just try every value starting there!
-    for x in range(target.max_x, 0, -1):
-        if target.contains(_fire(initial_p, Velocity(x, initial_y_speed), target)):
-            return x
-    else:
-        return None
-
-
-def _find_max_y_speed(initial_p: Point, target: Area) -> Velocity:
-    """Find the maximum possible initial speed in the y direction."""
+def _find_possible_velocities(initial_p: Point, target: Area) -> Iterator[Velocity]:
+    """Yield all initial velocities that hit the target.."""
     # When the projectile is passing through the line y=0 on its way down, its
     # speed in the y direction must be greater than the speed at which it was
-    # launched.
+    # launched.  So it's pointless trying any y speed greater than
+    # abs(target.min_y).
     #
-    # So (assuming that the target y range lies below the x axis) it's
-    # pointless trying any y speed greater than abs(target.min_y). This
-    # provides a definite upper bound; find the max value below this that
-    # works.
-    for y in range(abs(target.min_y), 0, -1):
-        if (x := _find_suitable_x_speed(initial_p, y, target)) is not None:
-            return Velocity(x, y)
-    else:
-        raise ValueError(
-            "Couldn't find any speed to hit {target} starting from {initial_p}"
-        )
+    # For the x direction, an upper bound is target.max_x (this time assuming
+    # the target area is positive), so just try every value starting there!
+    #
+    # All of this assumes that the x targets are positive and the y targets
+    # negative!
+    assert target.max_y < 0 and target.min_y < 0
+    assert target.max_x > 0 and target.min_x > 0
 
-
-def _find_max_height(initial_p: Point, target: Area) -> int:
-    """Find the maximum height that can be attained before finishing in the target."""
-    return max(
-        point.y
-        for point in _trajectory(
-            initial_p, _find_max_y_speed(initial_p, target), target
-        )
+    yield from (
+        Velocity(x, y)
+        for y in range(target.min_y, (-target.min_y) + 1)
+        for x in range(1, target.max_x + 1)
+        if target.contains(_fire(initial_p, Velocity(x, y), target))
     )
+
+
+def _find_max_height(initial_p: Point, initial_v: Velocity, target: Area) -> int:
+    """Find the maximum height attained on a trajectory."""
+    return max(point.y for point in _trajectory(initial_p, initial_v, target))
 
 
 def _parse_input(lines: Iterable[str]) -> Area:
@@ -134,7 +113,14 @@ def _parse_input(lines: Iterable[str]) -> Area:
 def main(argv: list[str]) -> None:
     with open(argv[0]) as f:
         target = _parse_input(f)
-        print(_find_max_height(Point(0, 0), target))
+        velocities = list(_find_possible_velocities(Point(0, 0), target))
+        print(
+            max(
+                _find_max_height(Point(0, 0), velocity, target)
+                for velocity in velocities
+            )
+        )
+        print(len(velocities))
 
 
 if __name__ == "__main__":
